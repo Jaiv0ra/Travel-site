2edf9c2397c7f3dc368fd5881e1050b1a8ded9d9
jaivora19@gamil.com <jaivora19@gmail.com>
2023-04-14 11:49:47 +0530

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,35 @@
-const autoprefixer = require('autoprefixer')
-const path=require('path')
-const postCSSPlugins=[
-    require('postcss-import'),
-    require('postcss-mixins'),
-   require('postcss-simple-vars'),
-    require('postcss-nested'),
-    require('autoprefixer')
+const path = require('path')
+
+const postCSSPlugins = [
+  require('postcss-import'),
+  require('postcss-mixins'),
+  require('postcss-simple-vars'),
+  require('postcss-nested'),
+  require('autoprefixer')
 ]
+
 module.exports = {
-    entry: './app/assets/scripts/App.js',
-    output:{
-        filename: 'bundled.js',
-        path: path.resolve(__dirname,'app')
+  entry: './app/assets/scripts/App.js',
+  output: {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'app')
+  },
+  devServer: {
+    before: function(app, server) {
+      server._watch('./app/**/*.html')
     },
-    devServer: {
-        // before:function(app,server) {
-        //     server._watch('./app/**/*.html')
-        // },
-        onBeforeSetupMiddleware: function (devServer) {
-            devServer.app.get("./app/**/*.html", function (req, res) {
-              res.json({ custom: "response" });
-            });
-          },
-        static: path.join(__dirname,'app'),
-        hot:true,
-        port:3000,
-        host:'0.0.0.0'
-    },
-    mode:'development',
-    module:{
-        rules: [
-            {
-                test: /\.css$/i,
-                use:['style-loader','css-loader',{loader:'postcss-loader', options:{postcssOptions:{plugins: postCSSPlugins}}}]
-            }
-        ]
-    }
+    contentBase: path.join(__dirname, 'app'),
+    hot: true,
+    port: 3000,
+    host: '0.0.0.0'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader?url=false", { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }]
+      }
+    ]
+  }
 }
